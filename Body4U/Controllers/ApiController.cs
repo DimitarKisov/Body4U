20e4c7b4f3b3b361890780a6b653c2f040b528ce
Dimitar Kisov <dimitar.kisov@unrealsoft.bg>
2025-10-24 22:12:00 +0300

    Refactor API to focus on membership features
    Removed weather-related functionality, including `WeatherForecastController` and `WeatherForecast` classes. Added a project reference to `Body4U.Membership.Application` in `Body4U.Membership.Api.csproj`.
    
    Introduced `ApiController` as an abstract base class to centralize MediatR integration. Added `MembersController` with a `Create` endpoint to handle member creation using the mediator pattern.
    
    These changes shift the project toward a modular architecture focused on membership-related features.

---
+++ Body4U/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Body4U.Membership.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
---
+++ Body4U/Controllers/MembersController.cs
@@ -0,0 +1,14 @@
+﻿using Body4U.Membership.Application.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Body4U.Membership.Api.Controllers
+{
+    public class MembersController : ApiController
+    {
+        public async Task<ActionResult<Guid>> Create(CreateMemberCommand command)
+        {
+            var id =  await Mediator.Send(command);
+            return CreatedAtAction("Get", new { id }, id);
+        }
+    }
+}
